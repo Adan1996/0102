0a74ef5df4fded8ba4d360682d791dc2c1b255b5
Ubuntu <ubuntu@ip-172-31-37-78.ap-southeast-1.compute.internal>
2021-04-09 17:14:11 +0000

    memperbaiki teams

--- models/teams.py
+++ models/teams.py
@@ -5,9 +5,11 @@ class Teams(models.Model):
     _name = 'booking_service.teams'
     _description = 'Teams'
 
-    team_name = fields.Char(string='Team Name')
+    team_name = fields.Char(string='Team Name', required=True)
     team_leader = fields.Many2one(comodel_name='res.users', string='Team Leader')
-    employee_ids = fields.One2many(comodel_name='booking_service.employees', inverse_name='name', string="Employees")
+    employee_ids = fields.One2many(comodel_name='booking_service.employees', inverse_name='employee_id', string="Employees")
+    equipment_ids = fields.One2many(comodel_name='booking_service.equipments', inverse_name='equipment_id', string='Equipments')
+    
     
     
     
@@ -20,6 +22,19 @@ class Employees(models.Model):
     _description = "Employees"
 
     name = fields.Many2one(comodel_name='res.users', string='Employee', required=True)
+    employee_id = fields.Many2one(comodel_name='booking_service.teams', string='employee_id')
+
+class Equipments(models.Model):
+    _name = 'booking_service.equipments'
+    _description = 'Equipments'
+
+    product = fields.Many2one(comodel_name='product.template', string='Equipments', domain="[('is_an_equipment', '=', True)]")
+    serial_no = fields.Many2one(comodel_name='stock.production.lot', string='Serial No')
+    
+    equipment_id = fields.Many2one(comodel_name='booking_service.teams', string='equipment_id')
+    
+    
+    
     
     
 
